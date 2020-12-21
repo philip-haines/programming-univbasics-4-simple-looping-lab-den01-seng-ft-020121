10dba7dcf28f6e135a5f894f06c16e8dbf7d5964
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-21 18:06:06 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -29,11 +29,11 @@ def output_array(array)
 end
 
 def return_string_array(array)
-  new_string = ""
+  new_array = []
   count = 0
   
   while count < array.length do 
-    array[count].to_s
+    new_array << array[count].to_s
     count += 1
   end
   
