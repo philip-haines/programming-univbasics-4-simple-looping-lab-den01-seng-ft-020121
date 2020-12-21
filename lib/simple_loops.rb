deace66afeb00cb4c3dbbe87e88ef29370fe37c4
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-21 17:54:28 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,10 +1,17 @@
 # Write your methods here
-def loop_message_five_times(string)
+def loop_message_five_times(message)
   count = 0
   
   while count < 5
-    puts string
+    puts message
     count += 1
   end
+  
+end
+
+def loop_message_n_times(message, n)
+  count = 0
+  
+  while count < n
   
 end
