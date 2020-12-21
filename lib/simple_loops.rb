8e5e31a2a0cd075ca7773d62f131417b6d9b547b
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-21 17:59:23 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,7 +2,7 @@
 def loop_message_five_times(message)
   count = 0
   
-  while count < 5
+  while count < 5 do
     puts message
     count += 1
   end
@@ -12,8 +12,17 @@ end
 def loop_message_n_times(message, limit)
   count = 0
   
-  while count < limit
+  while count < limit do
     puts message 
+    
+    count += 1
+  end
+end
+
+def output_array(array)
+  count = 0
+  while count < array.length do 
+    puts array[count]
     
     count += 1
   end
